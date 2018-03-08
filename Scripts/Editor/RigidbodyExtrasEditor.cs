9d85387720134372b8326d1319099d0c34926069
Jürgen Heiling <juergen.heiling@gmail.com>
2018-03-08 12:35:21 +0100

    added RigidbodyExtras

---
+++ Scripts/Editor/RigidbodyExtrasEditor.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+
+
+namespace Extras
+{
+    [CustomEditor(typeof(RigidbodyExtras))]
+    public class RigidbodyExtrasEditor : Editor
+    {
+        static readonly GUIContent _stopLabel = new GUIContent("Stop");
+        static readonly GUIContent _resetLabel = new GUIContent("Reset");
+        static readonly GUIContent _velocityLabel = new GUIContent("Velocity");
+        static readonly GUIContent _maxDepenetrationVelocityLabel = new GUIContent("Max Depenetration Velocity");
+        static readonly GUIContent _angularVelocityLabel = new GUIContent("Angular Velocity");
+        static readonly GUIContent _maxAngularVelocityLabel = new GUIContent("Max Angular Velocity");
+        static readonly GUIContent _sleepThresholdLabel = new GUIContent("Sleep Threshold");
+        static readonly GUIContent _detectCollisionsLabel = new GUIContent("Detect Collisions");
+        static readonly GUIContent _setCentreOfMassLabel = new GUIContent("Set Centre Of Mass");
+        static readonly GUIContent _centreOfMassLabel = new GUIContent("Centre Of Mass");
+        static readonly GUIContent _setInertiaTensorLabel = new GUIContent("Set Inertia Tensor");
+        static readonly GUIContent _inertiaTensorLabel = new GUIContent("Inertia Tensor");
+        static readonly GUIContent _setSolverIterationsLabel = new GUIContent("Set Solver Iterations");
+        static readonly GUIContent _solverIterationsLabel = new GUIContent("Solver Iterations");
+        static readonly GUIContent _setSolverVelocityIterationsLabel = new GUIContent("Set Solver Velocity Iterations");
+        static readonly GUIContent _solverVelocityIterationsLabel = new GUIContent("Solver Velocity Iterations");
+
+        Rigidbody _rigidbody;
+        RigidbodyExtras _rigidbodyExtras;
+
+        void OnEnable()
+        {
+            _rigidbodyExtras = target as RigidbodyExtras;
+            _rigidbody = _rigidbodyExtras.GetComponent<Rigidbody>();
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayoutExtras.Horizontal(() =>
+            {
+                _rigidbodyExtras.Velocity = EditorGUILayout.Vector3Field(_velocityLabel, _rigidbodyExtras.Velocity);
+                if (Application.isPlaying && GUILayout.Button(_stopLabel)) _rigidbodyExtras.Velocity = Vector3.zero;
+            });
+
+            _rigidbodyExtras.MaxDepenetrationVelocity = EditorGUILayout.FloatField(_maxDepenetrationVelocityLabel, _rigidbodyExtras.MaxDepenetrationVelocity);
+
+            EditorGUILayoutExtras.Horizontal(() =>
+            {
+                _rigidbodyExtras.AngularVelocity = EditorGUILayout.Vector3Field(_angularVelocityLabel, _rigidbodyExtras.AngularVelocity);
+                if (Application.isPlaying && GUILayout.Button(_stopLabel)) _rigidbodyExtras.AngularVelocity = Vector3.zero;
+            });
+
+            _rigidbodyExtras.MaxAngularVelocity = EditorGUILayout.FloatField(_maxAngularVelocityLabel, _rigidbodyExtras.MaxAngularVelocity);
+
+            _rigidbodyExtras.SleepThreshold = EditorGUILayout.FloatField(_sleepThresholdLabel, _rigidbodyExtras.SleepThreshold);
+
+            _rigidbodyExtras.DetectCollisions = EditorGUILayout.Toggle(_detectCollisionsLabel, _rigidbodyExtras.DetectCollisions);
+
+            if (!Application.isPlaying) _rigidbodyExtras.SetCentreOfMass = EditorGUILayout.Toggle(_setCentreOfMassLabel, _rigidbodyExtras.SetCentreOfMass);
+            if (Application.isPlaying || _rigidbodyExtras.SetCentreOfMass)
+            {
+                EditorGUILayoutExtras.Horizontal(() =>
+                {
+                    _rigidbodyExtras.CentreOfMass = EditorGUILayout.Vector3Field(_centreOfMassLabel, _rigidbodyExtras.CentreOfMass);
+                    if (Application.isPlaying && GUILayout.Button(_resetLabel)) _rigidbody.ResetCenterOfMass();
+                });
+            }
+
+            if (!Application.isPlaying) _rigidbodyExtras.SetInertiaTensor = EditorGUILayout.Toggle(_setInertiaTensorLabel, _rigidbodyExtras.SetInertiaTensor);
+            if (Application.isPlaying || _rigidbodyExtras.SetInertiaTensor)
+            {
+                EditorGUILayoutExtras.Horizontal(() =>
+                {
+                    _rigidbodyExtras.InertiaTensor = EditorGUILayout.Vector3Field(_inertiaTensorLabel, _rigidbodyExtras.InertiaTensor);
+                    if (Application.isPlaying && GUILayout.Button(_resetLabel)) _rigidbody.ResetInertiaTensor();
+                });
+            }
+
+            if (!Application.isPlaying) _rigidbodyExtras.SetSolverIterations = EditorGUILayout.Toggle(_setSolverIterationsLabel, _rigidbodyExtras.SetSolverIterations);
+            if (Application.isPlaying || _rigidbodyExtras.SetSolverIterations)
+            {
+                EditorGUILayoutExtras.Horizontal(() =>
+                {
+                    _rigidbodyExtras.SolverIterations = EditorGUILayout.IntField(_solverIterationsLabel, _rigidbodyExtras.SolverIterations);
+                    if (Application.isPlaying && GUILayout.Button(_resetLabel)) _rigidbody.solverIterations = Physics.defaultSolverIterations;
+                });
+            }
+
+            if (!Application.isPlaying) _rigidbodyExtras.SetSolverVelocityIterations = EditorGUILayout.Toggle(_setSolverVelocityIterationsLabel, _rigidbodyExtras.SetSolverVelocityIterations);
+            if (Application.isPlaying || _rigidbodyExtras.SetSolverVelocityIterations)
+            {
+                EditorGUILayoutExtras.Horizontal(() =>
+                {
+                    _rigidbodyExtras.SolverVelocityIterations = EditorGUILayout.IntField(_solverVelocityIterationsLabel, _rigidbodyExtras.SolverVelocityIterations);
+                    if (Application.isPlaying && GUILayout.Button(_resetLabel)) _rigidbody.solverVelocityIterations = Physics.defaultSolverVelocityIterations;
+                });
+            }
+        }
+
+        public override bool RequiresConstantRepaint()
+        {
+            return true;
+        }
+    }
+}
---
+++ Scripts/RigidbodyExtras.cs
@@ -0,0 +1,249 @@
+﻿using UnityEngine;
+
+
+
+namespace Extras
+{
+    /// <summary>
+    /// Allows you to serialize additional Rigidbody properties.
+    /// </summary>
+    [AddComponentMenu("Extras/RigidbodyExtras")]
+    [RequireComponent(typeof(Rigidbody))]
+    public class RigidbodyExtras : MonoBehaviour
+    {
+        [SerializeField] Vector3 _velocity;
+        [SerializeField] float _maxDepenetrationVelocity = Mathf.Infinity;
+        [SerializeField] Vector3 _angularVelocity;
+        [SerializeField] float _maxAngularVelocity = 7;
+        [SerializeField] float _sleepThreshold = .005f;
+        [SerializeField] bool _detectCollisions = true;
+        [SerializeField] bool _setCentreOfMass;
+        [SerializeField] Vector3 _centreOfMass;
+        [SerializeField] bool _setInertiaTensor;
+        [SerializeField] Vector3 _inertaTensor = new Vector3(.1666667f, .1666667f, .1666667f);
+        [SerializeField] bool _setSolverIterations;
+        [SerializeField] int _solverIterations = 6;
+        [SerializeField] bool _setSolverVelocityIterations;
+        [SerializeField] int _solverVelocityIterations = 1;
+        Rigidbody _rigidbody;
+
+        public Vector3 Velocity
+        {
+            get
+            {
+                return Application.isPlaying ? _rigidbody.velocity : _velocity;
+            }
+
+            set
+            {
+                if (Application.isPlaying) _rigidbody.velocity = value;
+                else _velocity = value;
+            }
+        }
+
+        public float MaxDepenetrationVelocity
+        {
+            get
+            {
+                return Application.isPlaying ? _rigidbody.maxDepenetrationVelocity : _maxDepenetrationVelocity;
+            }
+
+            set
+            {
+                if (Application.isPlaying) _rigidbody.maxDepenetrationVelocity = value;
+                else _maxDepenetrationVelocity = value;
+            }
+        }
+
+        public Vector3 AngularVelocity
+        {
+            get
+            {
+                return Application.isPlaying ? _rigidbody.angularVelocity : _angularVelocity;
+            }
+
+            set
+            {
+                if (Application.isPlaying) _rigidbody.angularVelocity = value;
+                else _angularVelocity = value;
+            }
+        }
+
+        public float MaxAngularVelocity
+        {
+            get
+            {
+                return Application.isPlaying ? _rigidbody.maxAngularVelocity : _maxAngularVelocity;
+            }
+
+            set
+            {
+                if (value >= 0)
+                {
+                    if (Application.isPlaying) _rigidbody.maxAngularVelocity = value;
+                    else _maxAngularVelocity = value;
+                }
+            }
+        }
+
+        public float SleepThreshold
+        {
+            get
+            {
+                return Application.isPlaying ? _rigidbody.sleepThreshold : _sleepThreshold;
+            }
+
+            set
+            {
+                if (Application.isPlaying) _rigidbody.sleepThreshold = value;
+                else _sleepThreshold = value;
+            }
+        }
+
+        public bool DetectCollisions
+        {
+            get
+            {
+                return Application.isPlaying ? _rigidbody.detectCollisions : _detectCollisions;
+            }
+
+            set
+            {
+                if (Application.isPlaying) _rigidbody.detectCollisions = value;
+                else _detectCollisions = value;
+            }
+        }
+
+        public bool SetCentreOfMass
+        {
+            get
+            {
+                return _setCentreOfMass;
+            }
+
+            set
+            {
+                _setCentreOfMass = value;
+            }
+        }
+
+        public Vector3 CentreOfMass
+        {
+            get
+            {
+                return Application.isPlaying ? _rigidbody.centerOfMass : _centreOfMass;
+            }
+
+            set
+            {
+                if (Application.isPlaying) _rigidbody.centerOfMass = value;
+                else _centreOfMass = value;
+            }
+        }
+
+        public bool SetInertiaTensor
+        {
+            get
+            {
+                return _setInertiaTensor;
+            }
+
+            set
+            {
+                _setInertiaTensor = value;
+            }
+        }
+
+        public Vector3 InertiaTensor
+        {
+            get
+            {
+                return Application.isPlaying ? _rigidbody.inertiaTensor : _inertaTensor;
+            }
+
+            set
+            {
+                if (value.x > 0f && value.y > 0f && value.z > 0f)
+                {
+                    if (Application.isPlaying) _rigidbody.inertiaTensor = value;
+                    else _inertaTensor = value;
+                }
+            }
+        }
+
+        public bool SetSolverIterations
+        {
+            get
+            {
+                return _setSolverIterations;
+            }
+
+            set
+            {
+                _setSolverIterations = value;
+            }
+        }
+
+        public int SolverIterations
+        {
+            get
+            {
+                return Application.isPlaying ? _rigidbody.solverIterations : _solverIterations;
+            }
+
+            set
+            {
+                if (value > 0)
+                {
+                    if (Application.isPlaying) _rigidbody.solverIterations = value;
+                    else _solverIterations = value;
+                }
+            }
+        }
+
+        public bool SetSolverVelocityIterations
+        {
+            get
+            {
+                return _setSolverVelocityIterations;
+            }
+
+            set
+            {
+                _setSolverVelocityIterations = value;
+            }
+        }
+
+        public int SolverVelocityIterations
+        {
+            get
+            {
+                return Application.isPlaying ? _rigidbody.solverVelocityIterations : _solverVelocityIterations;
+            }
+
+            set
+            {
+                if (value > 0)
+                {
+                    if (Application.isPlaying) _rigidbody.solverVelocityIterations = value;
+                    else _solverVelocityIterations = value;
+                }
+            }
+        }
+
+        void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            _rigidbody.velocity = _velocity;
+            _rigidbody.maxDepenetrationVelocity = _maxDepenetrationVelocity;
+            _rigidbody.angularVelocity = _angularVelocity;
+            _rigidbody.maxAngularVelocity = _maxAngularVelocity;
+            _rigidbody.sleepThreshold = _sleepThreshold;
+            _rigidbody.detectCollisions = _detectCollisions;
+            if (_setCentreOfMass) _rigidbody.centerOfMass = _centreOfMass;
+            if (_setInertiaTensor) _rigidbody.inertiaTensor = _inertaTensor;
+            if (_setSolverIterations) _rigidbody.solverIterations = _solverIterations;
+            if (_setSolverVelocityIterations) _rigidbody.solverVelocityIterations = _solverVelocityIterations;
+        }
+    }
+}

cf470847e68dd52943f8204905870cc75eb32d07
Jürgen Heiling <juergen.heiling@gmail.com>
2018-03-08 12:34:56 +0100

    added EditorGUILayoutExtras

---
+++ Scripts/Editor/EditorGUILayoutExtras.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEditor;
+
+
+
+namespace Extras
+{
+    public static class EditorGUILayoutExtras
+    {
+        public static void Horizontal(Action inner)
+        {
+            EditorGUILayout.BeginHorizontal();
+            inner();
+            EditorGUILayout.EndHorizontal();
+        }
+
+        public static void Vertical(Action inner)
+        {
+            EditorGUILayout.BeginVertical();
+            inner();
+            EditorGUILayout.EndVertical();
+        }
+    }
+}

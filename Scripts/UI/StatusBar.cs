8264d726d4a38d8405b4522367813ecfb6494114
jheiling <juergen.heiling@gmail.com>
2019-02-26 23:42:43 +0100

    added UI.StatusBar

---
+++ Scripts/UI/StatusBar.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Extras.UI
+{
+    [AddComponentMenu(nameof(Extras) + "/" + nameof(UI) + "/" + nameof(StatusBar))]
+    [RequireComponent(typeof(RectTransform))]
+    public class StatusBar : MonoBehaviour
+    {
+        [SerializeField] float _current = 100;
+        [SerializeField] float _maximum = 100;
+
+        public float Current
+        {
+            get => _current;
+            set
+            {
+                _current = value;
+                SetWidth();
+            }
+        }
+
+        public float Maximum
+        {
+            get => _maximum;
+            set
+            {
+                _maximum = value;
+                SetWidth();
+            }
+        }
+
+        void SetWidth()
+        {
+            var maximumWidth = transform.parent.GetComponent<RectTransform>().rect.width;
+            GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, _maximum == 0 ? 0 : _current / _maximum * maximumWidth);
+        }
+    } 
+}
